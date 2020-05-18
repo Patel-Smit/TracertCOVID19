dd798769d3614508090561ac17ad97a25accae46
Smitkumar Patel <30504086+Patel-Smit@users.noreply.github.com>
2020-05-18 16:40:39 -0400

    STATISTICS DESIGN AND FIREBASE UPDATED

--- app/src/main/java/com/smit/tracertcovid_19/frag_statistics.java
+++ app/src/main/java/com/smit/tracertcovid_19/frag_statistics.java
@@ -6,23 +6,58 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.EventListener;
 
 public class frag_statistics extends Fragment {
 
+
+    private DatabaseReference mDatabaseReference;
+    TextView tested,totalCases,deaths;
     ImageView imageMap;
-    String imageMapURl = "https://replicationindex.files.wordpress.com/2020/03/image-19.png?w=1024";
+    String imageMapURl = "http://www.smitpatel.tech/TracertCOVID19/covidmap.png";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_statistics,container,false);
         imageMap = (ImageView)v.findViewById(R.id.iv_map);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("dataStats");
+        tested = (TextView) v.findViewById(R.id.tv_tested);
+        totalCases = (TextView) v.findViewById(R.id.tv_totalCases);
+        deaths = (TextView) v.findViewById(R.id.tv_deaths);
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String strtested = dataSnapshot.child("tested").getValue().toString();
+                tested.setText(strtested);
+                String strtotalcases = dataSnapshot.child("totalCases").getValue().toString();
+                totalCases.setText(strtotalcases);
+                String strdeaths = dataSnapshot.child("deaths").getValue().toString();
+                deaths.setText(strdeaths);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         Picasso.get().load(imageMapURl).into(imageMap);
         return v;
     }
+
 }
